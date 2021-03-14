5284642add49cdea1af65b02763131e7aeb6da70
Eddie <eddie.ch.89d@gmail.com>
2021-03-14 17:15:15 +0100

    Repository tests

--- src/test/java/se/lexicon/jpa_assignment/repository/RecipeCategoryRepositoryTest.java
+++ src/test/java/se/lexicon/jpa_assignment/repository/RecipeCategoryRepositoryTest.java
@@ -11,6 +11,7 @@ import se.lexicon.jpa_assignment.entity.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @DataJpaTest
@@ -31,7 +32,7 @@ public class RecipeCategoryRepositoryTest {
         List<RecipeCategory> recipeCategoryList = new ArrayList<>();
         ingredient = new Ingredient(1, "Flour");
         recipe = new Recipe(1, "Recipe Test");
-        recipeIngredient = new RecipeIngredient(ingredient, 10.4, Measurement.DL, recipe);
+        recipeIngredient = new RecipeIngredient("123e4567-e89b-12d3-a456-556642440000", ingredient, 10.4, Measurement.DL, recipe);
         List<RecipeIngredient> recipeIngredientList = new ArrayList<>();
         recipeIngredientList.add(recipeIngredient);
         recipeInstruction = new RecipeInstruction("Bake in the oven 10 - 12 minutes");
@@ -55,6 +56,14 @@ public class RecipeCategoryRepositoryTest {
         //testObject.save(recipeCategory);
         List<RecipeCategory> recipeCategoryList = testObject.findAll();
         Assertions.assertEquals(1, recipeCategoryList.size());
+    }
+
+
+    @Test
+    @DisplayName("Test find by id")
+    public void test_find_by_id() {
+        Optional<RecipeCategory> findByIdRC = testObject.findById(1);
+        Assertions.assertEquals("Bread", findByIdRC.get().getCategory());
     }
 
 
