96d00c43447233f496c2f63ee28184d410fd56e8
Eddie <eddie.ch.89d@gmail.com>
2021-03-14 14:23:31 +0100

    RecipeInstructionRepository test

--- src/main/java/se/lexicon/jpa_assignment/repository/RecipeInstructionRepository.java
+++ src/main/java/se/lexicon/jpa_assignment/repository/RecipeInstructionRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 import se.lexicon.jpa_assignment.entity.RecipeInstruction;
 
 import java.util.List;
+import java.util.Optional;
 
 
 public interface RecipeInstructionRepository extends CrudRepository<RecipeInstruction, String> {
     List<RecipeInstruction> findAll();
+    Optional<RecipeInstruction> findById(String instructionsId);
 }
--- src/test/java/se/lexicon/jpa_assignment/repository/RecipeInstructionRepositoryTest.java
+++ src/test/java/se/lexicon/jpa_assignment/repository/RecipeInstructionRepositoryTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import se.lexicon.jpa_assignment.entity.RecipeInstruction;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @DataJpaTest
@@ -40,6 +41,15 @@ public class RecipeInstructionRepositoryTest {
         Assertions.assertEquals("Add some pepper and salt after your taste", recipeInstructionList.get(2).getInstructions());
         Assertions.assertEquals("Bread need to be in the oven 10 minutes", recipeInstructionList.get(1).getInstructions());
         Assertions.assertEquals("You need to cook chicken at least 20 minutes", recipeInstructionList.get(0).getInstructions());
+    }
+
+
+    @Test
+    @DisplayName("Test find by id")
+    public void test_find_by_id() {
+        List<RecipeInstruction> recipeInstructionList = testObject.findAll();
+        Optional<RecipeInstruction> recipeInstructionOptional = testObject.findById(recipeInstructionList.get(0).getInstructionsId());
+        Assertions.assertEquals("You need to cook chicken at least 20 minutes", recipeInstructionOptional.get().getInstructions());
     }
 
 
